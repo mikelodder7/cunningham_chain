a6672c5ff93b08fd7ac42f3759ced12e9f641efe
Mike <mike@blueaxis.com>
2018-03-29 07:35:01 -0600

    Added more findings

--- src/chain.rs
+++ src/chain.rs
@@ -28,7 +28,7 @@ pub struct CunninghamChain {
     bits: usize,
     length: usize,
     kind: CunninghamKind,
-    starting_prime: String
+    starting_number: String
 }
 
 impl CunninghamChain {
@@ -106,7 +106,7 @@ impl CunninghamChain {
             CunninghamChain {
                 bits: starting_prime.bit_length(),
                 length: primes.len(),
-                starting_prime: starting_prime.to_str_radix(10),
+                starting_number: starting_prime.to_str_radix(10),
                 kind: CunninghamKind::FIRST
             }
         )
@@ -166,7 +166,7 @@ impl CunninghamChain {
             CunninghamChain {
                 bits: starting_prime.bit_length(),
                 length: primes.len(),
-                starting_prime: starting_prime.to_str_radix(10),
+                starting_number: starting_prime.to_str_radix(10),
                 kind: CunninghamKind::SECOND
             }
         )
@@ -233,7 +233,7 @@ impl CunninghamChain {
                 bits: seed.bit_length(),
                 length: numbers.len(),
                 kind: CunninghamKind::BITWIN,
-                starting_prime: numbers.front().unwrap().to_str_radix(10)
+                starting_number: numbers.front().unwrap().to_str_radix(10)
             }
         )
     }
